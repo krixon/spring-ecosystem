e44992fd21b99a4aafc8ba6339adfdaf07114caa
Karl Rixon <karlrixon@gmail.com>
2018-09-04 20:42:15 +0100

    Add more API tests and corresponding documentation

--- profiling-service/src/test/java/com/krixon/ecosystem/profiling/ApiDocumentation.java
+++ profiling-service/src/test/java/com/krixon/ecosystem/profiling/ApiDocumentation.java
@@ -11,18 +11,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.restdocs.payload.JsonFieldType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import javax.servlet.RequestDispatcher;
 
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
 
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
@@ -31,6 +32,7 @@ import static org.springframework.restdocs.hypermedia.HypermediaDocumentation.li
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.patch;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.post;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.*;
@@ -74,7 +76,7 @@ public class ApiDocumentation
         mockMvc
             .perform(get("/error")
                 .requestAttr(RequestDispatcher.ERROR_STATUS_CODE, 400)
-                .requestAttr(RequestDispatcher.ERROR_REQUEST_URI, "/notes")
+                .requestAttr(RequestDispatcher.ERROR_REQUEST_URI, "/fields")
                 .requestAttr(RequestDispatcher.ERROR_MESSAGE, "The constraint 'http://localhost:8080/constraints/123' does not exist."))
             .andDo(print())
             .andExpect(status().isBadRequest())
@@ -124,26 +126,139 @@ public class ApiDocumentation
     }
 
     @Test
-    public void fieldsCreateExample() throws Exception
+    public void fieldCreateExample() throws Exception
     {
-        Map<String, String> field = new HashMap<>();
-        field.put("id", "ebcc68c0-7c0e-45fc-a0cd-b7c631f45dd4");
-        field.put("panelId", "ff1cc98a-1e03-48b3-8f21-f89be1918f16");
-        field.put("name", "Date of Birth");
+        Map<String, String> field = createFieldData(
+            "ebcc68c0-7c0e-45fc-a0cd-b7c631f45dd4",
+            "ff1cc98a-1e03-48b3-8f21-f89be1918f16",
+            "Date of Birth"
+        );
 
-        mockMvc.perform(
-            post("/fields")
-                .contentType(MediaTypes.HAL_JSON)
-                .content(objectMapper.writeValueAsString(field)))
-            .andExpect(status().isCreated())
+        String fieldLocation = postField(field)
             .andDo(document("fields-create-example",
                 requestFields(
-                    fieldWithPath("id").description("The unique identifier of the field."),
-                    fieldWithPath("panelId").description("The unique identifier of the panel which owns the field."),
-                    fieldWithPath("name").description("A human-friendly name for the field."))));
+                    idField(),
+                    panelIdField(),
+                    nameField())))
+            .andReturn()
+                .getResponse()
+                .getHeader("Location");
+
+        verifyFieldResource(fieldLocation, field);
     }
 
-    private void createField(String id, String panelId, String name) {
+    @Test
+    public void fieldGetExample() throws Exception
+    {
+        Map<String, String> field = createFieldData(
+            "ebcc68c0-7c0e-45fc-a0cd-b7c631f45dd4",
+            "ff1cc98a-1e03-48b3-8f21-f89be1918f16",
+            "Date of Birth"
+        );
+
+        String fieldLocation = postField(field)
+            .andReturn()
+                .getResponse()
+                .getHeader("Location");
+
+        verifyFieldResource(fieldLocation, field)
+            .andDo(print())
+            .andDo(document("field-get-example",
+                links(
+                    linkWithRel("self").description("Canonical link for this <<resources-field,field>>."),
+                    linkWithRel("field").description("This <<resources-field,field>>.")),
+                responseFields(
+                    idField(),
+                    panelIdField(),
+                    nameField(),
+                    subsectionWithPath("_links").description("<<resources-field-links,Links>> to other resources."))));
+    }
+
+    @Test
+    public void fieldUpdateExample() throws Exception {
+
+        Map<String, String> field = createFieldData(
+            "ebcc68c0-7c0e-45fc-a0cd-b7c631f45dd4",
+            "ff1cc98a-1e03-48b3-8f21-f89be1918f16",
+            "Date of Birth"
+        );
+
+        String fieldLocation = postField(field)
+            .andReturn()
+                .getResponse()
+                .getHeader("Location");
+
+        verifyFieldResource(fieldLocation, field);
+
+        Map<String, String> fieldUpdate = new HashMap<>();
+        fieldUpdate.put("name", "New Name");
+
+        this.mockMvc.perform(
+            patch(fieldLocation)
+                .contentType(MediaTypes.HAL_JSON)
+                .content(objectMapper.writeValueAsString(fieldUpdate)))
+            .andExpect(status().isNoContent())
+            .andDo(document("field-update-example",
+                requestFields(
+                    nameField().optional())));
+    }
+
+    private FieldDescriptor idField()
+    {
+        return fieldWithPath("id")
+            .description("The unique identifier of the field.")
+            .type(JsonFieldType.STRING);
+    }
+
+    private FieldDescriptor panelIdField()
+    {
+        return fieldWithPath("panelId")
+            .description("The unique identifier of the panel which owns the field.")
+            .type(JsonFieldType.STRING);
+    }
+
+    private FieldDescriptor nameField()
+    {
+        return fieldWithPath("name")
+            .description("A human-friendly name for the field.")
+            .type(JsonFieldType.STRING);
+    }
+
+    private ResultActions postField(final Map<String, String> field) throws Exception
+    {
+        return mockMvc
+            .perform(
+                post("/fields")
+                    .contentType(MediaTypes.HAL_JSON)
+                    .content(objectMapper.writeValueAsString(field)))
+            .andExpect(status().isCreated());
+    }
+
+    private ResultActions verifyFieldResource(final String fieldLocation, final Map<String, String> field) throws Exception
+    {
+        return mockMvc
+            .perform(get(fieldLocation))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("id", is(field.get("id"))))
+            .andExpect(jsonPath("panelId", is(field.get("panelId"))))
+            .andExpect(jsonPath("name", is(field.get("name"))))
+            .andExpect(jsonPath("_links.self.href", is(fieldLocation)))
+            .andExpect(jsonPath("_links.field.href", is(fieldLocation)));
+    }
+
+    private Map<String, String> createFieldData(final String id, final String panelId, final String name)
+    {
+        Map<String, String> field = new HashMap<>();
+
+        field.put("id", id);
+        field.put("panelId", panelId);
+        field.put("name", name);
+
+        return field;
+    }
+
+    private void createField(String id, String panelId, String name)
+    {
         this.fieldRepository.save(new Field(id, panelId, name));
     }
 }
